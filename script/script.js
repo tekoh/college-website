5baa10767c7ef29cd3c405595ed00c04d7103a90
max <max.0x71@gmail.com>
2022-01-27 19:04:39 +0000

    a

--- script/script.js
+++ script/script.js
@@ -1,3 +1,15 @@
-function submitForm() {
-    
+function hey(e) {
+    e.preventDefault()
+    const email = e.target.elements.email.value
+    console.log(`given email: ${email}`)
+
+    const regex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+
+    const match = email.match(regex)
+
+    if (match < 1) {
+        alert("invalid email")
+    } else {
+        location.href = "./confirmation.html"
+    }
 }
