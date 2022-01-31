52ded108ab2642f01e925fc5dd862b75e9abc6c5
max <29782397+tekoh@users.noreply.github.com>
2022-01-31 10:09:42 +0000

    a

--- script/script.js
+++ script/script.js
@@ -13,3 +13,46 @@ function submit(e) {
         location.href = "./confirmation.html"
     }
 }
+
+$(window).on("load", () => {
+    $("body").removeClass("preload")
+
+    if (!$("#countdown-text")[0]) return
+
+    const countdown = () => {
+        $("#countdown-text")[0].innerText = `${MStoTime(new Date(Date.parse("25/05/2003")) - Date.now())} until shop opening!`
+    }
+
+    countdown()
+
+    setInterval(countdown, 1000);
+})
+
+// converts milliseconds to human understandable format
+function MStoTime(ms) {
+    const days = Math.floor(ms / (24 * 60 * 60 * 1000))
+    const daysms = ms % (24 * 60 * 60 * 1000)
+    const hours = Math.floor(daysms / (60 * 60 * 1000))
+    const hoursms = ms % (60 * 60 * 1000)
+    const minutes = Math.floor(hoursms / (60 * 1000))
+    const minutesms = ms % (60 * 1000)
+    const sec = Math.floor(minutesms / 1000)
+
+    let output = ""
+
+    if (days > 0) {
+        output += `${days} day${days > 1 ? "s" : ""} `
+    }
+
+    if (hours > 0) {
+        output += `${hours} hour${hours > 1 ? "s" : ""} `
+    }
+
+    if (minutes > 0) {
+        output += `${minutes} minute${minutes > 1 ? "s" : ""} `
+    }
+
+    output += `${sec} second${sec > 1 ? "s" : ""}`
+
+    return output
+}
