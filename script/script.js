c5446d8e6f3be0a5a6ecee8d619f7faf0ae56c5a
max <29782397+tekoh@users.noreply.github.com>
2022-01-31 10:13:37 +0000

    a

--- script/script.js
+++ script/script.js
@@ -20,6 +20,8 @@ $(window).on("load", () => {
 
     if (!$("#countdown-text")[0]) return
 
+    console.log(new Date(Date.parse("25/05/2022")))
+
     const countdown = () => {
         $("#countdown-text")[0].innerText = `${MStoTime(new Date(Date.parse("25/05/2022")).getTime() - Date.now())} until shop opening!`
     }
